5801ee2ddfe551b7e376657e3cbab4f0854f6cf6
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-06 14:59:28 +0530

    DB migration and prisma call to DB

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hi there");
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+import dotenv from "dotenv";
+dotenv.config();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const client = new PrismaClient({ adapter });
+
+async function createUser() {
+  await client.user.create({
+    data: {
+      username: "ActonidePrime",
+      password: "123123",
+      age: 23,
+    },
+  });
+}
+createUser();
