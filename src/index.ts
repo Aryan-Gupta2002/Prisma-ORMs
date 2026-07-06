e3e6ad4b758472cd8b5e00fca01a1350a90d44bc
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-06 16:03:17 +0530

    prisma request using Express

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "./generated/prisma/client.js";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const client = new PrismaClient({ adapter });
+const app = express();
 
-async function createUser() {
-  await client.user.create({
-    data: {
-      username: "ActonidePrime",
-      password: "123123",
-      age: 23,
-    },
+app.get("/users", async (req, res) => {
+  const users = await client.user.findMany();
+  res.json({
+    users,
   });
-}
-findUser();
+});
 
-async function findUser() {
-  const res = await client.user.findFirst({
+app.get("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const todos = await client.user.findFirst({
     where: {
-      id: 1,
+      id: parseInt(id),
     },
-    include: {
+    select: {
       todos: true,
+      username: true,
+      password: true,
     },
   });
-  console.log(res);
-}
+  res.json({ todos });
+});
+app.listen(3000);
